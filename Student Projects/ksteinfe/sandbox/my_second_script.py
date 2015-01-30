15b02fe4021386e3f6fc8f01c88cd68cca882e92
Kyle Steinfeld <ksteinfe@gmail.com>
2015-01-29 19:14:12 -0800

    translation stuff

--- Student Projects/ksteinfe/sandbox/my_second_script.py
+++ Student Projects/ksteinfe/sandbox/my_second_script.py
@@ -1,3 +1,14 @@
-from translate import Translator
-trans = Translator(to_lang="es")
-print trans.translate("child")
+import translate
+
+langs = ["de"]
+
+fname = "play.txt"
+with open(fname) as f: content = str(f.readlines())
+
+for lang in langs:
+    trans = translate.Translator(to_lang=lang)
+    print trans.translate("child")
+
+    transcontent = trans.translate(content).encode("utf8","ignore")
+    with open("out_"+lang+".txt", "w") as text_file:
+        text_file.write(transcontent)   
