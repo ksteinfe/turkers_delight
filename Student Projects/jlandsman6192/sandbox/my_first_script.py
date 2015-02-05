4de46a0cf1b7cb9b7719911318d003d9aa13982a
jlandsman6192 <jlandsman6192@gmail.com>
2015-02-05 13:50:42 -0800

    img_1024

---
+++ Student Projects/jlandsman6192/sandbox/my_first_script.py
@@ -0,0 +1,3 @@
+from translate import Translator
+trans = Translator(to_lang="es")
+print trans.translate("hello")
---
+++ Student Projects/jlandsman6192/sandbox/test.py
@@ -0,0 +1,6 @@
+foods = ['kyle':'apple','peng':'watermelon']
+foods['jared'] = 'pineapple'
+foods['dinnertimes'] = {'breakfast':10,'lunch'1,'dinner':6}
+
+import json
+print json.dumps(foods)
